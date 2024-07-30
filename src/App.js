7ac62c5894beec99f57a733e048df3c4ffaec990
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-07-30 02:49:07 +0100

    create react app and install tailwind

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import AgeCalculator from './components/ageCalculator';
+
+function App() {
+  return (
+    <div className="text-4xl font-bold text-blue-600">
+      <AgeCalculator />
+    </div>
+  );
+}
+
+export default App;
