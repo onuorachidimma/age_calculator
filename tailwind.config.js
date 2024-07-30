bc6f14bcb888933b163ba87bec2fb82802baa593
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-07-30 06:36:20 +0100

    customize tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: '#377AE1', 
+        grey: '#EEEEEE', 
+        deepBlue: '#202474', 
+        deepGrey: '#C4C4C2', 
+        lightBlue: '#5A88BA', 
+        deeperBlue: '#1B56D5', 
+      },
+    },
   },
   plugins: [],
 }
